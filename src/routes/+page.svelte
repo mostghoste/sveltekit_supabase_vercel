3126e9f8e1a538db5080d7509ba71d30cad3a7c2
mostghoste <sarunas.jacikas+github@gmail.com>
2024-05-20 13:51:12 +0300

    Add auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+	$: ({ countries } = data);
+</script>
+
+<h1>Welcome to Supabase!</h1>
+<ul>
+	{#each countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul>

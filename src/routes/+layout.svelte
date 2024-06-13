11ce22585ff057c070a7e7d4419d2de5d365740e
mostghoste <sarunas.jacikas+github@gmail.com>
2024-06-13 13:33:48 +0300

    Add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { goto, invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import '../app.css';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -16,4 +17,6 @@
 	});
 </script>
 
-<slot />
+<main class="flex justify-center flex-col items-center h-screen">
+	<slot />
+</main>

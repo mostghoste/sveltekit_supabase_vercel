ff62fac44b3025834146e63812fc5e8b0d2226bf
mostghoste <sarunas.jacikas+github@gmail.com>
2024-06-14 14:23:33 +0300

    Style for prod

--- src/routes/turnyrai/[id]/+page.svelte
+++ src/routes/turnyrai/[id]/+page.svelte
@@ -46,7 +46,7 @@
 
 {#if tournament_participant}
 	<p class="text-sm">Tu šiame turnyre <strong>dalyvauji</strong></p>
-	<Predictions {unpredictedMatchups}></Predictions>
+	<!-- <Predictions {unpredictedMatchups}></Predictions> -->
 {:else}
 	<p class="text-sm mt-2 mb-1">Tu šiame turnyre dar <strong>nedalyvauji</strong></p>
 	<h3 class="text-left">Dalyvaudamas turnyre įsipareigoji:</h3>
@@ -83,52 +83,70 @@
 {#if tournament_participant}
 	<h2>Mano spėjimai</h2>
 	<h3>Dar neatlikti spėjimai ({unpredictedMatchups.length})</h3>
-	<h4></h4>
 	{#if unpredictedMatchups && unpredictedMatchups.length > 0}
-		{#each unpredictedMatchups as matchup}
-			<form use:enhance method="post" action="?/makePrediction">
-				<input type="hidden" name="home_name" value={matchup.team_home} />
-				<input type="hidden" name="away_name" value={matchup.team_away} />
+		<section class="flex flex-col gap-2">
+			{#each unpredictedMatchups as matchup}
+				<form
+					use:enhance
+					method="post"
+					action="?/makePrediction"
+					class="bg-base-200 p-2 rounded-box flex flex-col gap-2"
+				>
+					<input type="hidden" name="home_name" value={matchup.team_home} />
+					<input type="hidden" name="away_name" value={matchup.team_away} />
 
-				<input type="hidden" name="matchup_id" value={matchup.id} />
-				<div>
-					<label>Kas laimės?:</label>
+					<input type="hidden" name="matchup_id" value={matchup.id} />
 					<div>
+						<label>Kas laimės?:</label>
+						<div>
+							<input
+								type="radio"
+								id="team_home_{matchup.id}"
+								name="selected_team"
+								value="home_win"
+								required
+							/>
+							<label for="team_home_{matchup.id}">{matchup.team_home}</label>
+						</div>
+						<div>
+							<input
+								type="radio"
+								id="team_away_{matchup.id}"
+								name="selected_team"
+								value="away_win"
+								required
+							/>
+							<label for="team_away_{matchup.id}">{matchup.team_away}</label>
+						</div>
+						<div>
+							<input type="radio" id="tie_{matchup.id}" name="selected_team" value="tie" required />
+							<label for="tie_{matchup.id}">Lygiosios</label>
+						</div>
+					</div>
+					<div class="flex flex-col text-left">
+						<label for="score_home_{matchup.id}">{matchup.team_home} taškai:</label>
 						<input
-							type="radio"
-							id="team_home_{matchup.id}"
-							name="selected_team"
-							value="home_win"
-							required
+							class="input input-bordered"
+							type="number"
+							id="score_home_{matchup.id}"
+							name="score_home"
+							placeholder="0"
 						/>
-						<label for="team_home_{matchup.id}">{matchup.team_home}</label>
 					</div>
-					<div>
+					<div class="flex flex-col text-left">
+						<label for="score_away_{matchup.id}">{matchup.team_away} taškai:</label>
 						<input
-							type="radio"
-							id="team_away_{matchup.id}"
-							name="selected_team"
-							value="away_win"
-							required
+							class="input input-bordered"
+							type="number"
+							id="score_away_{matchup.id}"
+							name="score_away"
+							placeholder="0"
 						/>
-						<label for="team_away_{matchup.id}">{matchup.team_away}</label>
 					</div>
-					<div>
-						<input type="radio" id="tie_{matchup.id}" name="selected_team" value="tie" required />
-						<label for="tie_{matchup.id}">Lygiosios</label>
-					</div>
-				</div>
-				<div>
-					<label for="score_home_{matchup.id}">{matchup.team_home} taškai:</label>
-					<input type="number" id="score_home_{matchup.id}" name="score_home" placeholder="69" />
-				</div>
-				<div>
-					<label for="score_away_{matchup.id}">{matchup.team_away} taškai:</label>
-					<input type="number" id="score_away_{matchup.id}" name="score_away" placeholder="69" />
-				</div>
-				<button type="submit">Pateikti spėjimą</button>
-			</form>
-		{/each}
+					<button class="btn btn-success btn-sm" type="submit">Pateikti spėjimą</button>
+				</form>
+			{/each}
+		</section>
 	{:else}
 		<p>Visi galimi spėjimai atlikti</p>
 	{/if}

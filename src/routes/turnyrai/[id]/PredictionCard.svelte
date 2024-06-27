776b6e92e5510aecebf2b7f1015eb40d236d0a3b
mostghoste <sarunas.jacikas+github@gmail.com>
2024-06-27 05:08:05 +0300

    Fix chrome input

--- src/routes/turnyrai/[id]/PredictionCard.svelte
+++ src/routes/turnyrai/[id]/PredictionCard.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	export let matchup;
 	export let prediction;
-
 	export let onUpdateScore;
-
-	$: home_score_prediction = prediction.prediction_home;
-	$: away_score_prediction = prediction.prediction_away;
-
-	console.log(home_score_prediction);
-
 	export let tieAllowed = true;
+
+	let homeInput, awayInput;
+
+	$: home_score_prediction = prediction?.prediction_home ?? '';
+	$: away_score_prediction = prediction?.prediction_away ?? '';
+
+	const updateScore = () => {
+		home_score_prediction = homeInput.value.trim() !== '' ? parseInt(homeInput.value, 10) || 0 : '';
+		away_score_prediction = awayInput.value.trim() !== '' ? parseInt(awayInput.value, 10) || 0 : '';
+		onUpdateScore(home_score_prediction, away_score_prediction);
+	};
 </script>
 
 <article class="bg-base-200 rounded-box flex justify-between p-2 gap-2 text-sm">
@@ -23,50 +27,23 @@
 	>
 		{matchup.team_home}
 	</button>
-	<div class=" w-full justify-evenly flex flex-col">
-		<!-- <div></div> -->
+	<div class="w-full justify-evenly flex flex-col">
 		<div class="flex justify-evenly">
 			<input
-				bind:value={home_score_prediction}
-				on:input={() => onUpdateScore(home_score_prediction, away_score_prediction)}
+				bind:this={homeInput}
+				value={home_score_prediction}
+				on:input={updateScore}
 				class="input input-bordered input-sm w-12 text-center"
 				type="text"
 			/>
 			<input
-				bind:value={away_score_prediction}
-				on:input={() => onUpdateScore(home_score_prediction, away_score_prediction)}
+				bind:this={awayInput}
+				value={away_score_prediction}
+				on:input={updateScore}
 				class="input input-bordered input-sm w-12 text-center"
 				type="text"
 			/>
 		</div>
-		<!-- <div>
-			<button
-				on:click={() => {
-					home_score_prediction = 1;
-					away_score_prediction = 0;
-					onUpdateScore(home_score_prediction, away_score_prediction);
-				}}
-				class="btn btn-sm btn-secondary px-2">1:0</button
-			>
-			{#if tieAllowed}
-				<button
-					on:click={() => {
-						home_score_prediction = 1;
-						away_score_prediction = 1;
-						onUpdateScore(home_score_prediction, away_score_prediction);
-					}}
-					class="btn btn-sm btn-secondary px-2">1:1</button
-				>
-			{/if}
-			<button
-				on:click={() => {
-					home_score_prediction = 0;
-					away_score_prediction = 1;
-					onUpdateScore(home_score_prediction, away_score_prediction);
-				}}
-				class="btn btn-sm btn-secondary px-2">0:1</button
-			>
-		</div> -->
 	</div>
 	<button
 		on:click={() => {

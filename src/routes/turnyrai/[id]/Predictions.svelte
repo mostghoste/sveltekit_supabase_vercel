575aac068986e5f3b73cc45a8216115f2a940563
mostghoste <sarunas.jacikas+github@gmail.com>
2024-06-27 19:48:00 +0300

    Remove unnecessary code

--- src/routes/turnyrai/[id]/Predictions.svelte
+++ src/routes/turnyrai/[id]/Predictions.svelte
@@ -72,16 +72,17 @@
 
 	$: currentMatchup = unpredictedMatchups[currentlySelectedMatchup];
 	$: currentPrediction = predictions[currentlySelectedMatchup];
+	$: displayedMatchup = { ...currentMatchup };
 
-	// $: if (currentMatchup.team_home === 'TBD') {
-	// 	console.log('TBD HOME');
-	// }
+	$: if (currentMatchup.team_home === 'TBD') {
+		console.log('TBD HOME');
+	}
 
-	// $: if (currentMatchup.team_away === 'TBD') {
-	// 	console.log('TBD AWAY');
-	// 	console.log(unpredictedMatchups[currentlySelectedMatchup]);
-	// 	unpredictedMatchups[
-	// }
+	$: if (currentMatchup.team_away === 'TBD' && currentMatchup.away_previous) {
+		let votedWinner = predictions.find((p) => p.matchup_id === currentMatchup.id);
+		console.log(votedWinner);
+		// displayedMatchup = { ...displayedMatchup, team_away:  };
+	}
 
 	const tieAllowed = false;
 
@@ -157,7 +158,7 @@
 				<h4>{currentMatchup.type}</h4>
 			{/if}
 			<PredictionCard
-				matchup={unpredictedMatchups[currentlySelectedMatchup]}
+				matchup={displayedMatchup}
 				prediction={predictions[currentlySelectedMatchup]}
 				{tieAllowed}
 				{onUpdateScore}
@@ -211,54 +212,3 @@
 		{/if}
 	</section>
 {/if}
-
-<!-- <h2>Mano spėjimai</h2>
-<h3>Dar neatlikti spėjimai ({unpredictedMatchups.length})</h3>
-{#if unpredictedMatchups && unpredictedMatchups.length > 0}
-	{#each unpredictedMatchups as matchup}
-		<form use:enhance method="post" action="?/makePrediction">
-			<input type="hidden" name="home_name" value={matchup.team_home} />
-			<input type="hidden" name="away_name" value={matchup.team_away} />
-
-			<input type="hidden" name="matchup_id" value={matchup.id} />
-			<div>
-				<label>Kas laimės?:</label>
-				<div>
-					<input
-						type="radio"
-						id="team_home_{matchup.id}"
-						name="selected_team"
-						value="home_win"
-						required
-					/>
-					<label for="team_home_{matchup.id}">{matchup.team_home}</label>
-				</div>
-				<div>
-					<input
-						type="radio"
-						id="team_away_{matchup.id}"
-						name="selected_team"
-						value="away_win"
-						required
-					/>
-					<label for="team_away_{matchup.id}">{matchup.team_away}</label>
-				</div>
-				<div>
-					<input type="radio" id="tie_{matchup.id}" name="selected_team" value="tie" required />
-					<label for="tie_{matchup.id}">Lygiosios</label>
-				</div>
-			</div>
-			<div>
-				<label for="score_home_{matchup.id}">{matchup.team_home} taškai:</label>
-				<input type="number" id="score_home_{matchup.id}" name="score_home" placeholder="69" />
-			</div>
-			<div>
-				<label for="score_away_{matchup.id}">{matchup.team_away} taškai:</label>
-				<input type="number" id="score_away_{matchup.id}" name="score_away" placeholder="69" />
-			</div>
-			<button type="submit">Pateikti spėjimą</button>
-		</form>
-	{/each}
-{:else}
-	<p>Visi galimi spėjimai atlikti</p>
-{/if} -->

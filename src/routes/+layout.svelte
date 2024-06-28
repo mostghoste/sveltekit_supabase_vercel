a4703221d1957cadea9137a6fd5768d751040df4
mostghoste <sarunas.jacikas+github@gmail.com>
2024-06-28 22:18:38 +0300

    Beta 1.08

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -109,6 +109,6 @@
 		<section class="flex-grow flex justify-center flex-col prose">
 			<slot></slot>
 		</section>
-		<footer class="text-xs text-gray-600"><p>@mostghoste 2024 | Beta 1.07</p></footer>
+		<footer class="text-xs text-gray-600"><p>@mostghoste 2024 | Beta 1.08</p></footer>
 	</main>
 </div>
